62f67b55f043079a4ba3ec25388208500e670234
Timocop <Timocop@hotmail.de>
2021-10-27 16:56:32 +1300

    Release batch

---
+++ CreateRelease.bat
@@ -0,0 +1,26 @@
+@echo off
+
+IF NOT EXIST build goto no_build
+
+IF NOT EXIST ".\release\testing" mkdir ".\release\PSMoveService\testing"
+
+copy /Y ".\build\src\psmoveclient\Release\*.dll" ".\release\PSMoveService\"
+copy /Y ".\build\src\psmoveservice\Release\*.exe" ".\release\PSMoveService\"
+copy /Y ".\build\src\psmoveconfigtool\Release\*.exe" ".\release\PSMoveService\"
+
+copy /Y ".\build\src\psmoveclient\Release\*.dll" ".\release\PSMoveService\testing\"
+copy /Y ".\build\src\tests\Release\*.exe" ".\release\PSMoveService\testing\"
+
+pause
+goto exit
+
+:no_build
+echo No build directory found!
+goto failure
+
+:exit
+EXIT /B 0
+
+:failure
+pause
+EXIT /B 1
