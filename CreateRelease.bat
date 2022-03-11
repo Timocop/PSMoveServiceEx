6c250c24879507f89e945ff9aa49445396fdb36c
Timocop <Timocop@hotmail.de>
2022-03-11 15:37:41 +1300

    Update release batch

--- CreateRelease.bat
+++ CreateRelease.bat
@@ -7,11 +7,18 @@ IF EXIST ".\release\PSMoveService\" rmdir /S /Q ".\release\PSMoveService\"
 IF NOT EXIST ".\release\PSMoveService\" mkdir ".\release\PSMoveService\"
 IF NOT EXIST ".\release\PSMoveService\testing\" mkdir ".\release\PSMoveService\testing\"
 
+IF NOT EXIST ".\release\PSMoveService\assets\calibration\" mkdir ".\release\PSMoveService\assets\calibration\"
+
+:: Copy misc calibration files to psmoveconfigtool.
+xcopy /S /I /Y ".\misc\calibration" ".\release\PSMoveService\assets\calibration"
+
+:: Move files to release.
 copy /Y ".\build\src\psmoveclient\Release\*.dll" ".\release\PSMoveService\"
 copy /Y ".\build\src\psmoveservice\Release\*.exe" ".\release\PSMoveService\"
 copy /Y ".\build\src\psmoveconfigtool\Release\*.exe" ".\release\PSMoveService\"
 xcopy /S /I /Y ".\src\psmoveconfigtool\assets" ".\release\PSMoveService\assets"
 
+:: Move testing binaries to testing directory.
 copy /Y ".\build\src\psmoveclient\Release\*.dll" ".\release\PSMoveService\testing\"
 copy /Y ".\build\src\tests\Release\*.exe" ".\release\PSMoveService\testing\"
 
