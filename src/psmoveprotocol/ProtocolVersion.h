95771b68124618aa8a5e6fdd0bcaa4bf839455c8
Timocop <Timocop@hotmail.de>
2022-12-04 09:15:12 +1300

    New Version

--- src/psmoveprotocol/ProtocolVersion.h
+++ src/psmoveprotocol/ProtocolVersion.h
@@ -10,8 +10,8 @@
 
 // Current version of this release
 #define PSM_RELEASE_VERSION_MAJOR		0
-#define PSM_RELEASE_VERSION_MINOR		18
-#define PSM_RELEASE_VERSION_BUILD		1
+#define PSM_RELEASE_VERSION_MINOR		19
+#define PSM_RELEASE_VERSION_BUILD		0
 #define PSM_RELEASE_VERSION_REVISION	0
 
 /// "Product.Major-Phase Minor.Release.Hotfix"
